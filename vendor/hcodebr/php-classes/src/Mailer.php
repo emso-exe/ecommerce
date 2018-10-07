4be942609d8dee488912c8c2211ade4ceb7823bc
Estênio Mariano <emso.exe@gmail.com>
2018-10-07 17:32:19 -0300

    Admin - User - Search and pagination

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -7,7 +7,7 @@ use Rain\Tpl;
 class Mailer {
 
 	const USERNAME = "dev.emso.exe@gmail.com";
-	const PASSWORD = "Osme2891";
+	const PASSWORD = "";
 	const NAME_FROM = "Hcode Store";
 
 	private $mail;
